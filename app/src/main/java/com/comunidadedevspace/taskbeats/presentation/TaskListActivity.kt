2d9cace05564b7c576c5ced87636c890c2f0f6e3
jaqviana <jaquelineviana84@gmail.com>
2023-05-01 17:05:19 -0400

    create viewmodel

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
@@ -11,16 +11,12 @@ import android.view.View
 import android.widget.LinearLayout
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import com.comunidadedevspace.taskbeats.R
-import com.comunidadedevspace.taskbeats.data.AppDataBase
 import com.comunidadedevspace.taskbeats.data.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
 import java.io.Serializable
 
 class TaskListActivity : AppCompatActivity() {
@@ -34,31 +30,18 @@ class TaskListActivity : AppCompatActivity() {
         TaskListAdapter(::onListItemClicked )
     }
 
-    private val dataBase by lazy { //by lazy esse codigo so vai executar qdo eu precisar utilizar o database
-        Room.databaseBuilder(
-            applicationContext,
-            AppDataBase::class.java, "taskbeats-database" //eh o nome da nossa base de dados
-        ).build()
-
-    }
-    private val dao by lazy { //esse codigo so vai ser utilizado qdo for utilizar o dao
-        dataBase.taskDao()
+    private val viewModel: TaskListViewModel by lazy {
+        TaskListViewModel.create(application)
     }
 
-    private val startForResult = registerForActivityResult(
+     private val startForResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
         if (result.resultCode == Activity.RESULT_OK) {
-
             val data = result.data
             val taskAction = data?.getSerializableExtra(TASK_ACTION_RESULT) as TaskAction
-            val task: Task = taskAction.task
 
-            when (taskAction.actionType) {
-                ActionType.DELETE.name -> deleteByID(task.id)
-                ActionType.CREATE.name -> insertIntoDataBase(task)
-                ActionType.UPDATE.name -> updateIntoDataBase(task)
-            }
+            viewModel.execute(taskAction)
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,7 +49,7 @@ class TaskListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_task_list)
         setSupportActionBar(findViewById(R.id.toolbar)) //colocar o toolbar q criamos
 
-        listFromDataBase()
+
         ctnContent = findViewById(R.id.ctn_content)
 
 
@@ -88,42 +71,31 @@ class TaskListActivity : AppCompatActivity() {
     //Update
     //Delete
 
-
-    private fun insertIntoDataBase(task: Task) {
-        CoroutineScope(IO).launch {
-            dao.insert(task) //insiro a nova tarefa
-            listFromDataBase() //chamo a list de tarefas
-        }
-
+    //icializando o codigo
+    override fun onStart() {
+        super.onStart()
+        listFromDataBase()
     }
 
-    private fun updateIntoDataBase(task: Task) {
-        CoroutineScope(IO).launch {
-            dao.update(task) //update da tarefa
-            listFromDataBase() //chamo a list de tarefas
-        }
-
+    private fun deleteAll() {
+        val taskAction = TaskAction( null, ActionType.DELETE_ALL.name)
+       viewModel.execute(taskAction)
     }
 
-    private fun deleteAll(){
-        CoroutineScope(IO).launch {
-            dao.deleteALl()
-            listFromDataBase() //deleta no banco de dados e atuliza nossa lista do recyclereview
-        }
-    }
 
-    private fun deleteByID(id: Int){
-        CoroutineScope(IO).launch {
-            dao.deleteById(id)
-            listFromDataBase() //deleta no banco de dados e atuliza nossa lista do recyclereview
-        }
-    }
-    private fun listFromDataBase(){
-        CoroutineScope(IO).launch {
-            val myDataBaseList: List<Task> = dao.getAll()
-            adapter.submitList(myDataBaseList)
+    private fun listFromDataBase() {
+           //Observer
+            val listObserver = Observer<List<Task>>{ listTasks ->
+                if(listTasks.isEmpty()) {
+                    ctnContent.visibility = View.VISIBLE
+                } else {
+                    ctnContent.visibility = View.GONE
+                }
+                adapter.submitList(listTasks)
+            }
 
-        }
+            //Preciso linkar com o Live data
+            viewModel.taskListLiveData.observe(this@TaskListActivity, listObserver)
     }
 
     private fun showMassage(view: View, message: String){
@@ -163,13 +135,14 @@ class TaskListActivity : AppCompatActivity() {
 //CRUD (Create, Read, Update, Delete)
 enum class ActionType {
   DELETE,
+  DELETE_ALL,
   UPDATE,
   CREATE,
 }
 
 //enum acima nao serializar ele, passar de uma tela pra outra por isso q passo por uma string e comparo name
 data class TaskAction(
-    val task: Task,
+    val task: Task?,
     val actionType: String
 ) : Serializable
 
---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListViewModel.kt
@@ -0,0 +1,70 @@
+package com.comunidadedevspace.taskbeats.presentation
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.comunidadedevspace.taskbeats.TaskBeatsApplication
+import com.comunidadedevspace.taskbeats.data.Task
+import com.comunidadedevspace.taskbeats.data.TaskDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+//classe q preciso ter dependencia que eh a classe Dao
+class TaskListViewModel(private val taskDao: TaskDao): ViewModel() {
+
+    //assim tenho meu livedata no viewmodel
+    val taskListLiveData: LiveData<List<Task>> = taskDao.getAll()
+
+    fun execute(taskAction: TaskAction) {
+        when (taskAction.actionType) {
+            ActionType.DELETE.name -> deleteByID(taskAction.task!!.id)
+            ActionType.CREATE.name -> insertIntoDataBase(taskAction.task!!)
+            ActionType.UPDATE.name -> updateIntoDataBase(taskAction.task!!)
+            ActionType.DELETE_ALL.name -> deleteAll()
+        }
+    }
+
+    private fun deleteByID(id: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.deleteById(id)
+        }
+
+    }
+
+    private fun insertIntoDataBase(task: Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.insert(task) //insiro a nova tarefa
+
+        }
+
+    }
+
+    private fun updateIntoDataBase(task: Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.update(task) //update da tarefa
+
+        }
+
+    }
+
+    private fun deleteAll(){
+       viewModelScope.launch(Dispatchers.IO) {
+           taskDao.deleteALl()
+
+        }
+    }
+    companion object{
+
+        //essa fun vai me retornar/criar um viewmodel
+        fun create(application: Application):TaskListViewModel {
+            //para e pegar o database
+            val dataBaseInstance = (application as TaskBeatsApplication).getAppDataBase()
+            val dao = dataBaseInstance.taskDao()
+           //agora que tenho um task Dao vou conseguir criar um viewModel, para criar um viewModel preciso passar um dao como parametro de contrucao (dao)
+            return TaskListViewModel(dao)
+        }
+    }
+
+}
